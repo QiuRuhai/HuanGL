bd89ac88baf83667a7d61f1d183261a8e900f5c6
Qiu_Ruhai <qrh818272@gmail.com>
2026-05-14 18:36:24 +0800

    chore: remove unused includes from LightingPass.cpp

--- src/pipeline/passes/LightingPass.cpp
+++ src/pipeline/passes/LightingPass.cpp
@@ -7,8 +7,6 @@
 #include "../../renderer/Renderer.h"
 #include "../../scene/Scene.h"
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <stdexcept>
 
 namespace HuanGL {
 
