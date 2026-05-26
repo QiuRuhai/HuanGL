0985aec54ce091ab71a5a9cd6682c68e56c6272b
Qiu_Ruhai <qrh818272@gmail.com>
2026-05-26 16:00:34 +0800

    refactor: add IPipelineStage interface

---
+++ src/pipeline/IPipelineStage.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace HuanGL {
+
+class PipelineResources;
+struct FrameContext;
+
+class IPipelineStage {
+public:
+    virtual ~IPipelineStage() = default;
+    virtual const char* GetName() const = 0;
+    virtual void Init(int width, int height) = 0;
+    virtual void Resize(int width, int height) = 0;
+    virtual void Execute(PipelineResources& resources, const FrameContext& frame) = 0;
+};
+
+} // namespace HuanGL
